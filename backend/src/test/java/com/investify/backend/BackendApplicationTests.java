245e21a9d2783cdd1b569134122f3fba1516d0d3
Anirudhx234 <65822278+Anirudhx234@users.noreply.github.com>
2024-09-25 22:47:24 -0400

    Initial backend commit

---
+++ backend/src/test/java/com/investify/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
