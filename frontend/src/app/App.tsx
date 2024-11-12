7f023913417694a7adf3f2d0fca912737c5daa79
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-11-12 16:48:21 -0500

    Refactor initial commit

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,35 +1,3 @@
-import useAppSelector from "../hooks/useAppSelector";
-import twMerge from "../util/twMerge";
-import AppRouter from "./AppRouter";
-import Navbar from "../scenes/Navbar";
-import AppSidebar from "./AppSidebar";
-
 export default function App() {
-  const drawerMode = useAppSelector((state) => state.appRoute.args?.drawerMode);
-
-  return (
-    <div
-      className={twMerge("drawer", drawerMode === "open" && "lg:drawer-open")}
-    >
-      <input className="drawer-toggle" id="drawer" type="checkbox" />
-      <div className="drawer-content">
-        <div className="flex flex-col h-screen">
-          <header className="sticky top-0 z-30 flex h-16 w-full justify-center bg-base-100 bg-opacity-90 text-base-content shadow-sm backdrop-blur">
-            <Navbar />
-          </header>
-          <main className="flex-1 overflow-y-auto flex w-full max-w-[100vw] flex-col items-start justify-start ~px-2/6 py-8 text-base-content">
-            <AppRouter />
-          </main>
-        </div>
-      </div>
-      <div className="drawer-side z-40">
-        <label
-          htmlFor="drawer"
-          className="drawer-overlay"
-          aria-label="Close Menu"
-        ></label>
-        <AppSidebar />
-      </div>
-    </div>
-  );
+  return <>Hello World</>;
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,28 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./app/store";
-import LoadingPage from "./pages/LoadingPage";
-import { IconContext } from "react-icons";
-import { Router } from "wouter";
-
 import App from "./app/App";
-import ThemeManager from "./app/ThemeManager";
 
 /* render providers, app component */
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<LoadingPage />} persistor={persistor}>
-        <IconContext.Provider value={{}}>
-          <Router>
-            <App />
-            <ThemeManager />
-          </Router>
-        </IconContext.Provider>
-      </PersistGate>
-    </Provider>
+    <App />
   </StrictMode>,
 );
