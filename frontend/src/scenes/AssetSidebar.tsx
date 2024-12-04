96bd6c88d37fef0920c9ea99086f9bdaf80d0f2a
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-12-04 01:34:35 -0500

    Remove portfolio link from asset sidebar

--- frontend/src/scenes/AssetSidebar.tsx
+++ frontend/src/scenes/AssetSidebar.tsx
@@ -1,11 +1,6 @@
 import { LinksList } from "../components/LinksList";
-import { selectIsLoggedIn } from "../features/clientSlice";
-import { useAppSelector } from "../hooks/useAppSelector";
 
 const options = ["/general", "/line-chart", "/candle-chart", "/news"];
-const loggedInOptions = [...options, "/portfolio"];
 export function AssetSidebar() {
-  const isLoggedIn = useAppSelector(selectIsLoggedIn);
-
-  return <LinksList links={isLoggedIn ? loggedInOptions : options} />;
+  return <LinksList links={options} />;
 }
